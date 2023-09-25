8ec592c91874ed25a930b2510231cfd908e0404c
ikhyeon.kim <kih199212@gmail.com>
2023-09-26 00:09:35 +0900

    fix css url path

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "gyungju-grayscale": "url('./gyungju-grayscale.jpg')",
-        "gyungju-color": "url('./gyungju-color.JPG')",
+        "gyungju-grayscale": "url('../gyungju-grayscale.jpg')",
+        "gyungju-color": "url('../gyungju-color.JPG')",
       },
       colors: {
         "color-1": "#000928",
