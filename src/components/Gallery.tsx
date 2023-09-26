cd07ae573353252093061b071461f210731d41aa
ikhyeon.kim <kih199212@gmail.com>
2023-09-26 23:59:21 +0900

    gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,4 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import useSnappedIndex from "../hooks/useSnappedIndex";
 import { useScrollProgress } from "@toss/scroll-animation";
 import { useRef, RefObject, useState } from "react";
 const Gallery = () => {
@@ -10,6 +11,8 @@ const Gallery = () => {
     clip: true,
   });
 
+  const scrollContainer = useRef<HTMLDivElement>(null);
+  const currentIndex = useSnappedIndex(scrollContainer);
   // const translateY = (1 - scrollYProgress) * 100;
 
   const onClickLeft = () => {
@@ -19,51 +22,70 @@ const Gallery = () => {
     });
   };
 
-  const onClckRight = () => {
+  const onClickRight = () => {
     setActiveIndex((i) => {
       if (i > 4) return i;
       else return i + 1;
     });
   };
 
+  console.log(activeIndex);
+
   let beSeen = false;
   if (scrollYProgress >= 1) beSeen = true;
 
   return (
-    <div className="relative flex">
+    <div ref={ref as RefObject<HTMLDivElement>} className="relative flex">
       <div
-        ref={ref as RefObject<HTMLDivElement>}
+        ref={scrollContainer}
         style={{
           scrollSnapType: "x mandatory",
         }}
-        className="py-16 overflow-x-scroll scroll-container"
+        className="py-32 overflow-x-scroll scroll-container bg-gradient-to-b from-color-1 to-slate-800"
       >
-        <ul className="relative grid grid-flow-col gap-4 pl-12 list-none w-fit">
-          <div
-            className={`bg-red-400 w-72 h-80 transition-transform duration-500 delay-200 z-50 ${
+        <ul className="relative grid grid-flow-col gap-4 pl-20 pr-20 sm:pl-[30%] sm:pr-[30%] list-none w-fit">
+          <li
+            className={`gallery-item bg-red-400  flex justify-center w-60 h-80 transition-all duration-300 delay-50 z-50 ${
               beSeen ? "scale-100" : "scale-75"
-            }`}
-          ></div>
-          <div
-            className={`bg-yellow-400 w-80 h-80 transition-transform duration-500 delay-200 z-40 translate-x-[-300px] ${
-              beSeen ? "scale-100 translate-x-0" : "scale-75 translate-x-[-300px]"
-            }`}
-          ></div>
-          <div
-            className={`bg-green-400 w-[400px] z-30 h-80 transition-transform duration-500 delay-200 ${
+            } ${currentIndex === 0 ? "opacity-100" : "opacity-50"}`}
+          >
+            <img src="../bridge.JPG" />
+          </li>
+          <li
+            className={`gallery-item bg-green-400  flex justify-center w-[425px] z-30 h-80 transition-all duration-300 delay-50 ${
               beSeen ? "scale-100" : "scale-75"
-            }`}
-          ></div>
-          <div
-            className={`bg-blue-400 w-96 h-80 transition-transform duration-500 delay-200 z-20 ${
+            } ${currentIndex === 1 ? "opacity-100" : "opacity-50"}`}
+          >
+            <img src="../hwasung.JPG" />
+          </li>
+          <li
+            className={`gallery-item bg-purple-400  flex justify-center w-60 h-80 transition-all duration-300 delay-50 z-10 ${
               beSeen ? "scale-100" : "scale-75"
-            }`}
-          ></div>
-          <div
-            className={`bg-purple-400 w-72 h-80 transition-transform duration-500 delay-200 z-10 ${
+            } ${currentIndex === 2 ? "opacity-100" : "opacity-50"}`}
+          >
+            <img src="../omg.JPG" />
+          </li>
+          <li
+            className={`gallery-item bg-blue-400  flex justify-center w-[425px] h-80 transition-all duration-300 delay-50 z-20 ${
               beSeen ? "scale-100" : "scale-75"
-            }`}
-          ></div>
+            } ${currentIndex === 3 ? "opacity-100" : "opacity-50"}`}
+          >
+            <img src="../inwang.jpg" />
+          </li>
+          <li
+            className={`gallery-item bg-blue-400  flex justify-center w-44 h-80 transition-all duration-300 delay-50 z-20 ${
+              beSeen ? "scale-100" : "scale-75"
+            } ${currentIndex === 4 ? "opacity-100" : "opacity-50"}`}
+          >
+            <img src="../hmmm.JPG" />
+          </li>
+          <li
+            className={`gallery-item bg-blue-400  flex justify-center w-[425px] h-80 transition-all duration-300 delay-50 z-20 ${
+              beSeen ? "scale-100" : "scale-75"
+            } ${currentIndex === 5 ? "opacity-100" : "opacity-50"}`}
+          >
+            <img src="../brunch.JPG" />
+          </li>
         </ul>
       </div>
       <div className="absolute flex gap-2 bottom-4 right-4">
@@ -75,7 +97,7 @@ const Gallery = () => {
         </div>
         <div
           className="flex items-center justify-center bg-blue-900 rounded-full"
-          onClick={onClckRight}
+          onClick={onClickRight}
         >
           <ChevronRightIcon className="w-8 h-8 text-white" />
         </div>
---
+++ src/hooks/useSnappedIndex.tsx
@@ -0,0 +1,61 @@
+import { RefObject, useEffect, useRef, useState } from "react";
+const TICK = 50;
+
+const useSnappedIndex = (scrollContainer: RefObject<HTMLElement>) => {
+  const [snappedIndex, setSnappedIndex] = useState<number>(0);
+  const refNow = useRef(Date.now());
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!scrollContainer.current) return;
+      const now = Date.now();
+      if (now - refNow.current < TICK) {
+        refNow.current = now;
+        return;
+      }
+      const copiedContainer = scrollContainer.current;
+
+      const scrollLeft = copiedContainer.scrollLeft + 240;
+      const children = copiedContainer.getElementsByClassName("gallery-item");
+
+      console.log("fired");
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i] as HTMLElement;
+        const childLeft = child.offsetLeft;
+        const childWidth = child.offsetWidth;
+
+        console.log({
+          scrollLeft,
+          childLeft,
+          childWidth,
+          childLeftPlusChildWidth: childLeft + childWidth,
+          i,
+          firstCondition: scrollLeft < childLeft + childWidth,
+          secondCondition: scrollLeft >= childLeft,
+          scrollX: copiedContainer.offsetWidth,
+        });
+        if (scrollLeft < childLeft + childWidth && scrollLeft >= childLeft) {
+          setSnappedIndex(i);
+          break;
+        }
+      }
+
+      // console.log("hiiiiii", { scrollLeft, children });
+    };
+
+    if (!scrollContainer.current) return;
+    const copiedContainer = scrollContainer.current;
+    copiedContainer.addEventListener("scroll", handleScroll);
+
+    return () => {
+      copiedContainer.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollContainer]);
+
+  if (!scrollContainer) return 0;
+  if (!scrollContainer.current) return 0;
+
+  return snappedIndex;
+};
+
+export default useSnappedIndex;
